c89fc88df5518370d4267317f0b2f8370ecc38fc
aka <alexander.kaiser@igz.com>
2024-04-04 15:33:33 +0200

    [AKA]:
    Add:
    Mod: Erweitert um Scanner
    Ref:
    Fix:
    Rem:
    Rea:

--- src/Übungsaufgaben/Übungsaufgabe_1/FloatParser.java
+++ src/Übungsaufgaben/Übungsaufgabe_1/FloatParser.java
@@ -1,19 +1,37 @@
 package Übungsaufgaben.Übungsaufgabe_1;
 
+import java.util.Scanner;
+
 public class FloatParser {
     public static void main(String[] args) {
-        String sT1 = "-123.456f";   // erfolgreich
-        String sT2 = "+123.45A3";   // erfolgreich
-        String sT3 = "123.A";       // erfolgreich
-        String sT4 = "ABC";         // erfolgreich
-        String sT5 = "-0.123A";     // erfolgreich
-        String sT6 = "-123.0456f";  // erfolgreich
+        String sInput = "";
+        // String sT1 = " \t-123.456f"; // erfolgreich
+        // String sT2 = "-.45A3"; // erfolgreich
+        // String sT3 = "123.A"; // erfolgreich
+        // String sT4 = "ABC"; // erfolgreich
+        // String sT5 = "-0.123A"; // erfolgreich
+        // String sT6 = "-123.0456f"; // erfolgreich
+        Scanner oScan = new Scanner(System.in);
 
-        try {
-            FloatParser.parseMyFloat(sT6);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+        while (true) {
+            System.out.print("Bitte Text erfassen: ");
+            sInput = oScan.nextLine();
+
+            // Schleife verlassen
+            if (sInput.contains("y") || sInput.contains("Y")) {
+                break;
+            }
+
+            try {
+                FloatParser.parseMyFloat(sInput);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+
+            System.out.println("\n\n\n");
         }
+
+        oScan.close();
     }
 
     /**
@@ -34,7 +52,7 @@ public class FloatParser {
 
         System.out.printf("String zu Float! -> Umwandlung für [%s]:\n", sInput);
 
-        // Ermittlaung der ersten Zahl aus einem String
+        // Ermittlung der ersten Zahl aus einem String
         String[] aNumber = FloatParser.getFirstNumber(sInput);
 
         // String zum Float umwandeln
@@ -62,7 +80,7 @@ public class FloatParser {
             fReturn *= -1;
         }
 
-        System.out.printf("Umandlung erfolgreich durchgeführt. Ergebnis = [%f]:", fReturn);
+        System.out.printf("Umandlung erfolgreich durchgeführt. Ergebnis = [%f]", fReturn);
 
         return fReturn;
     }
@@ -82,14 +100,15 @@ public class FloatParser {
 
         // String-Objekt ist null
         // -> Logik nicht durchlaufen
-        if (sPreDec == null) {
+        if (sPreDec == null || sPreDec == "") {
             return fPreDec;
-        };
+        }
+        ;
 
         // Umwandlung in einen Int-Wert
         for (int i = 0; i < sPreDec.length(); i++) {
             iCurrValue = FloatParser.charToInt(sPreDec.charAt(i));
-            iCurrValue *= FloatParser.pow(10, (sPreDec.length() - (i+1)));
+            iCurrValue *= FloatParser.pow(10, (sPreDec.length() - (i + 1)));
             iSum += iCurrValue;
         }
 
@@ -117,15 +136,16 @@ public class FloatParser {
 
         // String-Objekt ist null
         // -> Logik nicht durchlaufen
-        if (sDec == null) {
+        if (sDec == null || sDec == "") {
             return fDec;
-        };
+        }
+        ;
 
         // Umwandlung in einen Int-Wert
         for (int i = 0; i < sDec.length(); i++) {
             iDiv *= 10;
             iCurrValue = FloatParser.charToInt(sDec.charAt(i));
-            iCurrValue *= FloatParser.pow(10, (sDec.length() - (i+1)));
+            iCurrValue *= FloatParser.pow(10, (sDec.length() - (i + 1)));
             iSum += iCurrValue;
         }
 
@@ -147,12 +167,12 @@ public class FloatParser {
      * @param iPow
      * @return
      */
-    private static int pow(int iVal, int iPow){
+    private static int pow(int iVal, int iPow) {
         if (iPow == 0) {
             return 1;
         }
 
-        return iVal * FloatParser.pow(iVal, iPow-1);
+        return iVal * FloatParser.pow(iVal, iPow - 1);
     }
 
     /**
@@ -213,22 +233,29 @@ public class FloatParser {
     private static String[] getFirstNumber(String sInput) throws Exception {
         // Deklaration
         char cCur;
-        int iFillCorrString = 0;
+        int iFillCorrString = 0,
+                iCount = 0;
         String[] aNumber = new String[2]; // -> Vorkomma- und Nachkommastellen getrennt darstellen
 
         // Vorkomma- und Nachkommastellen auf ein String-Array aufteilen
         for (int i = 0; i < sInput.length(); i++) {
             cCur = sInput.charAt(i);
 
+            // String beginnt mit SPACE/TABULATOR
+            // => Nächstes Zeichen des Strings betrachten
+            if (iCount == 0 && (cCur == ' ' || cCur == '\t')) {
+                continue;
+            }
+
             // String beginnt nicht mit einer Zahler oder +/-/,/.
             // -> Fehlermeldung schmeißen
-            if (i == 0 && !(Character.isDigit(cCur) || cCur == '+' || cCur == '-' || cCur == '.' || cCur == ',')) {
+            if (iCount == 0 && !(Character.isDigit(cCur) || cCur == '+' || cCur == '-' || cCur == '.' || cCur == ',')) {
                 throw new Exception(
                         String.format("ERROR:\nString [%s] startet nicht mit einer Zahl oder [+/-/,/.]!", sInput));
             }
             // Schleife verlassen, da ein Zeichen gekommen ist, welches nicht zu einem
             // float konvertiert werden könnte
-            else if (i > 0 && !(Character.isDigit(cCur) || cCur == '.' || cCur == ',')) {
+            else if (iCount > 0 && !(Character.isDigit(cCur) || cCur == '.' || cCur == ',')) {
                 break;
             }
             // Aktuell Zeichen ist ein ,/.
@@ -236,9 +263,11 @@ public class FloatParser {
             // wird
             else if (cCur == ',' || cCur == '.') {
                 iFillCorrString += 1;
+                iCount += 1;
                 continue;
             }
 
+            iCount += 1;
             aNumber[iFillCorrString] = (aNumber[iFillCorrString] == null ? "" + cCur : aNumber[iFillCorrString] + cCur);
         }
 
